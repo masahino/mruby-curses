e6a97bead37c4224fde5390ac438d63d95991bd7
masahino <masahino@gmail.com>
2016-03-08 21:47:51 +0900

    delete test

--- test/mrb_curses.rb
+++ test/mrb_curses.rb
@@ -2,16 +2,16 @@
 ## Curses Test
 ##
 
-assert("Curses#hello") do
-  t = Curses.new "hello"
-  assert_equal("hello", t.hello)
-end
-
-assert("Curses#bye") do
-  t = Curses.new "hello"
-  assert_equal("hello bye", t.bye)
-end
-
-assert("Curses.hi") do
-  assert_equal("hi!!", Curses.hi)
-end
+#assert("Curses#hello") do
+#  t = Curses.new "hello"
+#  assert_equal("hello", t.hello)
+#end
+#
+#assert("Curses#bye") do
+#  t = Curses.new "hello"
+#  assert_equal("hello bye", t.bye)
+#end
+#
+#assert("Curses.hi") do
+#  assert_equal("hi!!", Curses.hi)
+#end
